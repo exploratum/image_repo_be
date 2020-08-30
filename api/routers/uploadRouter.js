d00c0364a37d6653e469c5dded4578d6a2e9b8f1
Thierry Joux <tjoux@hotmail.com>
2020-08-30 15:51:28 -0700

    Create endpoint to get presigned url for user to upload directly to S3 bucket

--- api/routers/uploadRouter.js
+++ api/routers/uploadRouter.js
@@ -2,13 +2,48 @@ const express = require("express");
 const router = express.Router();
 const AWS = require("aws-sdk");
 
-router.post("/upload-request", async (req, res) => {
+/*******************************************************************************************************/
+/*               Provide presigned url allowing user to directly upload image to S3 bucket             */
+/*******************************************************************************************************/
+
+router.get("/request-upload-url", async (req, res) => {
     
-    const imgName = req.body;
+    const imgName = req.query.imgName;
 
-    console.log(imgName)
+    const s3 = new AWS.S3({
+        region: 'us-west-1'
+    })
 
-    res.status(200).json({url: "This is your signed url"})
+    const parameters = {
+        Bucket: process.env.BUCKET_NAME,
+        Key: imgName,
+        Expires: 60,
+        ContentType: "image/jpeg"
+    }
+
+
+    try {
+        url = await new Promise((resolve, reject) => {
+
+            s3.getSignedUrl('putObject', parameters, (err, url) => {
+    
+                if (err) {
+                    console.log(err)
+                    reject(err)
+                }
+                else {
+                    resolve(url)
+                }
+            })
+        })
+        res.status(200).json({url: url})
+    }
+
+    catch {
+        console.log("An error happened")
+        res.status(500).json({error: "can not get upload url"})
+    }
+    
 })
 
 module.exports = router;
