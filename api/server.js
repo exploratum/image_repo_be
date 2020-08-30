763169363c660a2f1af90cb6147ec73184a2267f
Thierry Joux <tjoux@hotmail.com>
2020-08-29 17:09:54 -0700

    create index.js and server.js file

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+const cors = require('cors');
+server.use(cors());
+
+server.use(logger);
+
+server.get('/', (req, res) => {
+    res.status(200).send("Image Repository API")
+})
+
+
+/**************************************/
+/*      Custom Middleware             */
+/**************************************/
+
+function logger(req, res, next) {
+    console.log(`Method ${req.method} requested at URL: ${req.url} on ${new Date().toISOString()}`);
+    next();
+}
+
+module.exports = server
